d638a3ec807a022e9077b62cb5aa674427f2d5cc
my PC <330634431@qq.com>
2022-05-14 13:31:06 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,70 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div class="hello">
+		<el-row class="m-t-20">
+			<el-col :span="16" :offset="4">
+				<el-input v-model="buy" placeholder="进价"></el-input>
+			</el-col>
+		</el-row>
+
+		<el-row class="m-t-20">
+			<el-col :span="16" :offset="4">
+				<el-input v-model="shipping" placeholder="运费"></el-input>
+			</el-col>
+		</el-row>
+
+		<el-button type="primary" class="m-t-20" @click="calc()">计算</el-button>
+		<el-button type="danger" class="m-t-20" @click="clear()">清除</el-button>
+
+		<el-table :data="tableData" height="550" border style="width: 100%">
+			<el-table-column prop="sell" label="售价">
+			</el-table-column>
+			<el-table-column prop="earn" label="利润">
+			</el-table-column>
+		</el-table>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+	export default {
+		name: 'HelloWorld',
+		data() {
+			return {
+				buy: '',
+				shipping: '',
+				tableData: []
+			}
+		},
+		methods: {
+			calc() {
+				if (this.buy && this.shipping) {
+					this.tableData = [];
+					var self = this;
+					for (var i = 700; i <= 2000; i += 50) {
+						var obj = new Object();
+						obj.sell = i;
+						obj.earn = i * 0.8 - self.buy - self.shipping;
+						self.tableData.push(obj);
+					}
+				} else {
+					this.$message({
+						message: '请输入完整信息',
+						type: 'warning'
+					});
+				}
+			},
+			
+			clear () {
+				this.buy = '';
+				this.shipping = '';
+				this.tableData = '';
+			}
+		}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+	.m-t-20 {
+		margin-bottom: 20px;
+	}
 </style>
